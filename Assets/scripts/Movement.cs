1c97cea3787dbb440422f7aa6b78150c5e4bfe66
Dconfair <149098044+Dconfair@users.noreply.github.com>
2024-04-05 12:04:07 -0400

    fixed movement speed

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -2,22 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
-    private Rigidbody2D rb; 
+    private Rigidbody2D rb;
+    public float movespeed=5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput=Input.GetAxis("Horizontal");
+        float verticalInput=Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector2(horizontalInput, verticalInput);
+        rb.velocity=new Vector2(horizontalInput,verticalInput)*movespeed;
     }
 }
