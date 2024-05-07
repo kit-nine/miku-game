3d3eb14c512790ba80b70b4ec955713f6b0576e6
Dconfair <149098044+Dconfair@users.noreply.github.com>
2024-05-07 12:23:13 -0400

    Level Switch Done

--- Assets/scripts/LevelSwitcher.cs
+++ Assets/scripts/LevelSwitcher.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LevelSwitcher : MonoBehaviour
 {
-    public void StartGame()
-    {
-        SceneManager.LoadScene("Level2");
+    public int sceneBuildIndex;
+
+    // Level move zoned enter, if collider is a player
+    // Move game to another scene
+    private void OnTriggerEnter2D(Collider2D other) {
+        print ("Trigger Entered");
+
+        // Could use other.GetComponent<player>() to see if the game object has a player component
+        // Tags work too. Maybe some players have diffrent script components?
+        if(other.tag == "Player"){
+            // Player entered, so move level
+            print("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
     }
 }
